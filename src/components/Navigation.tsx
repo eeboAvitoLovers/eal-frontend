f5167ced7f4972ec5f7f34e53967918e52195bcb
Vladislav Knyshov <inova136@gmail.com>
2024-04-28 09:20:15 +0200

    feat: update navigation ui

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,4 +1,14 @@
-import { Link, Box, Heading, Flex } from "@chakra-ui/react";
+import {
+  Link,
+  Box,
+  Heading,
+  Flex,
+  Menu,
+  MenuButton,
+  Button,
+  MenuList,
+  MenuItem,
+} from "@chakra-ui/react";
 import { FC } from "react";
 import { Link as RouterLink } from "react-router-dom";
 import { useAuth } from "../context/authContext";
@@ -23,30 +33,38 @@ const Navigation: FC = () => {
         <Link as={RouterLink} to="/auth">
           Аккаунт
         </Link>
-        <Link as={RouterLink} to="/check">
-          Проверить статус обращения
-        </Link>
-
-        {auth && (
-          <>
-            <Link as={RouterLink} to="/">
-              Создать обращение
-            </Link>
-            {auth.is_engineer && (
+        {auth && auth.is_engineer && (
+          <Link as={RouterLink} to="/analytics">
+            Аналитика
+          </Link>
+        )}
+        <Menu>
+          <MenuButton as={Button} rightIcon={<>▼</>} variant="outline">
+            Обращения
+          </MenuButton>
+          <MenuList>
+            <MenuItem as={RouterLink} to="/check">
+              Проверить статус обращения
+            </MenuItem>
+            {auth && (
               <>
-                <Link as={RouterLink} to="/tickets">
-                  Все обращения
-                </Link>
-                <Link as={RouterLink} to="/analytics">
-                  Аналитика
-                </Link>
-                <Link as={RouterLink} to="/tickets/my">
-                  Обращения в работе
-                </Link>
+                <MenuItem as={RouterLink} to="/">
+                  Создать обращение
+                </MenuItem>
+                {auth.is_engineer && (
+                  <>
+                    <MenuItem as={RouterLink} to="/tickets">
+                      Все обращения
+                    </MenuItem>
+                    <MenuItem as={RouterLink} to="/tickets/my">
+                      Обращения в работе
+                    </MenuItem>
+                  </>
+                )}
               </>
             )}
-          </>
-        )}
+          </MenuList>
+        </Menu>
       </Flex>
     </Flex>
   );
